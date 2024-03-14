9b9e07ca32f0ab6c1cbcf2833b025810e38f075e
evelinaoleinic <evelina.oleinic1@gmail.com>
2024-03-14 18:59:10 +0200

    tema15

--- tema.js
+++ tema.js
@@ -56,3 +56,38 @@ const func = ({denumire, tipul}) => {
 }
 func({denumire:'kitkat', tipul:'cu lapte'})
 
+// mai mult de o zi mai putin de o sapt 4days 10Header
+// mai mult de o ora si minute - 7hours si 50minutes 
+// mai putin de o ora - less thn an hour
+
+
+const time = 12456
+const week = 10080
+const day = 1440
+const hour = 60
+
+const timeFormat = (remaningTime) => {
+    if(remaningTime/week >= 1) {
+        const weekMark = Math.floor(remaningTime/week) !==1 ? 'weeks' : 'week'
+        const dayMark = (remaningTime%week)/day > 1 ? 'days' : 'day'
+        return `${Math.floor(remaningTime/week)}${weekMark}${Math.floor((remaningTime%week)/day)}${dayMark}`
+    }
+    if(remaningTime/day >= 1) {
+        const dayMark1 = Math.floor(remaningTime/day) !==1 ? 'days' : 'day'
+        const hourMark = (remaningTime%day)/hour > 1 ? 'hours' : 'hour'
+        return`${Math.floor(remaningTime/day)}${dayMark1}${Math.floor((remaningTime%day/hour))}${hourMark}`
+    }
+    if(remaningTime/hour >=1){
+        const hourMark1 = Math.floor(remaningTime/day) !==1 ? 'days' : 'day'
+        const minMark = remaningTime%hour >1 ? 'minutes' : 'minute'
+        return`${Math.floor(remaningTime/hour)}${hourMark1}${Math.floor(remaningTime%hour)}${minMark}`
+    }
+
+    if(remaningTime < hour) {
+        return 'less than an hour'
+
+    }
+
+}
+console.log(timeFormat(time))
+console.log(timeFormat(time))
